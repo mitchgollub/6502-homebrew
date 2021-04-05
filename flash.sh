d1a4607171f8675a45440ff6a35f5a2e9fc87525
Mitch Gollub <mgollub@live.com>
2021-04-04 21:57:21 -0400

    flash.sh spacing updates didn't work, reverting

--- flash.sh
+++ flash.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-./vasm/vasm6502_oldstyle \
-    -Fbin \ # Use binary output module
-    -dotdir \ # Use .directives
-    -wdc02 \  # Use WDC65c02 instruction set
-    src/robo-runner.s && \
+./vasm/vasm6502_oldstyle -Fbin -dotdir -wdc02 src/robo-runner.s && \
 minipro -p 'AT28C256' -w a.out
